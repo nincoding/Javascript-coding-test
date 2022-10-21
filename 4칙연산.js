b3d7ebb30abc2c97fd8ab698a6a99460d3ef8469
Han Su Ji <112839119+nincoding@users.noreply.github.com>
2022-10-21 20:45:13 +0900

    Add files via upload

---
+++ 4칙연산.js
@@ -0,0 +1,8 @@
+var input1 = prompt("숫자를 입력해 주세요");
+var input2 = prompt("숫자를 입력해 주세요");
+n1 = Number(input1);
+n2 = Number(input2);
+console.log(n1 + "더하기" + n2 +"="+(n1+n2));
+console.log(n1 + "빼기" + n2 +"="+(n1-n2));
+console.log(n1 + "나누기" + n2 +"="+(n1/n2));
+console.log(n1 + "곱하기" + n2 +"="+(n1*n2));
