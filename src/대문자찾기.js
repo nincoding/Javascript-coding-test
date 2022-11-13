200f2eed2e1996f40f0aa5fc71b5bdd1d59d420f
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2022-11-13 23:53:48 +0900

    :sparkles: feat: 특정 문자에서  대문자의 개수 찾기 기능 구현

---
+++ src/대문자찾기.js
@@ -0,0 +1,18 @@
+/*
+한 개의 문자열을 입력받아 해당 문자열에
+알파벳 대문자가 몇 개 있는지 알아내는 프로그램을 작성하세요.
+
+입력설명: 첫 줄에 문자열이 입력된다. 문자열의 길이는 100을 넘지 않습니다.
+출력설명: 첫 줄에 대문자의 개수를 출력한다.
+입력예제1: KoreaTimeGood
+출력예제1: 3
+*/
+
+let input = `KoreaTimeGood`
+function solution(input) {
+    input.split("").filter((v) => {
+       // v.toUpperCase
+    })
+    return input.length
+}
+console.log(solution(input));
