05c965c561a7320e510657b24d59c3834bc622b0
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2022-11-30 23:11:50 +0900

    feat: 격자판최대합 문제 풀이

---
+++ src/격자판최대합.js
@@ -0,0 +1,52 @@
+/*
+n*n의 격자판이 주어지면 각 행의 합, 각 열의 합, 두 대각선의 합 중
+가장 큰 합을 출력합니다.
+
+입력설명:
+첫 줄에 자연수 n이 주어진다.(1<=n<=50)
+두 번째 줄부터 n줄에 걸쳐 각 줄에 n개의 자연수가 주어진다.
+각 자연수는 100을 넘지 않는다.
+
+출력설명:
+최대합을 출력합니다.
+
+입력예제1:
+5
+10 13 10 12 15
+12 39 30 23 11
+11 25 50 53 15
+19 27 29 37 27
+19 13 30 13 19
+
+출력예제1:
+155
+*/
+function solution(arr){  
+  let answer=Number.MIN_SAFE_INTEGER;
+  let n=arr.length;
+  let sum1=sum2=0;
+  for(let i=0; i<n; i++){
+    sum1=sum2=0;
+    for(let j=0; j<n; j++){
+      sum1+=arr[i][j];
+      sum2+=arr[j][i];
+    }
+    answer=Math.max(answer, sum1, sum2);
+  }
+  sum1=sum2=0;
+  for(let i=0; i<n; i++){
+    sum1+=arr[i][i];
+    sum2+=arr[i][n-i-1];
+  }  
+  answer=Math.max(answer, sum1, sum2); 
+  return answer;
+}
+
+let arr=[
+  [10, 13, 10, 12, 15], 
+  [12, 39, 30, 23, 11],
+  [11, 25, 50, 53, 15],
+  [19, 27, 29, 37, 27],
+  [19, 13, 30, 13, 19]
+];
+console.log(solution(arr));
